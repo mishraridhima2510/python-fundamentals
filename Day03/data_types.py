1b2f3ee459906818b512c1e39741d65d3339817b
RIDHIMA MISHRA <mishraridhima65@gmail.com>
2026-07-03 17:01:22 +0530

    feat(day-03): add Python data types program

---
+++ Day03/data_types.py
@@ -0,0 +1,11 @@
+# Day 03 - Data Types
+
+name = "Tae"
+age = 19
+height = 6.0
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Student:", is_student)
