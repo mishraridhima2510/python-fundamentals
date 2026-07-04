04259b1b4c20ece8ecb49f11a781deffc918ca33
RIDHIMA MISHRA <mishraridhima65@gmail.com>
2026-07-04 10:48:12 +0530

    Remove misplaced if_else.py from repository root

--- if_else.py
+++
@@ -1,9 +0,0 @@
-age = int(input("Enter your age: "))
-
-if age >= 18:
-    print("You are eligible to vote.")
-else:
-    print("You are not eligible to vote.")
-
-# Author: Ridhima Mishra
-# Day 04 - Python Fundamentals
