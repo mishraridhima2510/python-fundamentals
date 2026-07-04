8feea37ca0daa66f6bf47a636059b365c3c3b1f1
RIDHIMA MISHRA <mishraridhima65@gmail.com>
2026-07-04 18:32:17 +0530

    Add Day04: Even or Odd Program

---
+++ even_odd.py
@@ -0,0 +1,10 @@
+
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print("The number is Even.")
+else:
+    print("The number is Odd.")
+
+# Author: Ridhima Mishra
+# Day 04 - Python Fundamentals
