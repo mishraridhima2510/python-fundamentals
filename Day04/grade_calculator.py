6967ae800b1be0f6ec9cf49ace2efbdbbcec8003
RIDHIMA MISHRA <mishraridhima65@gmail.com>
2026-07-04 18:41:00 +0530

    Add Day04: Grade Calculator using if-elif-else

---
+++ Day04/grade_calculator.py
@@ -0,0 +1,15 @@
+marks = int(input("Enter your marks (0-100): "))
+
+if marks >= 90:
+    print("Grade: A")
+elif marks >= 80:
+    print("Grade: B")
+elif marks >= 70:
+    print("Grade: C")
+elif marks >= 60:
+    print("Grade: D")
+else:
+    print("Grade: F")
+
+# Author: Ridhima Mishra
+# Day 04 - Python Fundamentals
