786411fc1d77617134a5ef28ebd7ba1d983db98e
RIDHIMA MISHRA <mishraridhima65@gmail.com>
2026-07-12 21:28:30 +0530

    Add mini project using tuple for student record

---
+++ Day08/student_record.py
@@ -0,0 +1,7 @@
+student = ("Ridhima", 101, "Cybersecurity", 8.81)
+
+print("----- Student Record -----")
+print("Name :", student[0])
+print("Roll No :", student[1])
+print("Branch :", student[2])
+print("CGPA :", student[3])
