f50b2b97bbf59c998dfd1a5fc38d96f422507a07
RIDHIMA MISHRA <mishraridhima65@gmail.com>
2026-07-15 09:25:07 +0530

    Add string methods examples

---
+++ Day10/04_string_methods.py
@@ -0,0 +1,9 @@
+text = " hello python "
+
+print(text.upper())
+print(text.lower())
+print(text.title())
+print(text.strip())
+print(text.replace("python", "world"))
+print(text.count("o"))
+print(text.find("python"))
