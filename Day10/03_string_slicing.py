b164645e47fb895303be6b116b1ecf8cb22af635
RIDHIMA MISHRA <mishraridhima65@gmail.com>
2026-07-15 09:24:31 +0530

    Add string slicing program

---
+++ Day10/03_string_slicing.py
@@ -0,0 +1,6 @@
+text = "Programming"
+
+print("First 6 Characters :", text[:6])
+print("Characters from Index 3 :", text[3:])
+print("Characters 3 to 8 :", text[3:8])
+print("Reverse :", text[::-1])
