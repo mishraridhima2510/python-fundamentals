c8b3899a7194f2b5270c9077b5267ec0cf71266b
RIDHIMA MISHRA <mishraridhima65@gmail.com>
2026-07-19 20:26:24 +0530

    Add finally block example

---
+++ Day14/04_finally_block.py
@@ -0,0 +1,11 @@
+# Finally Block
+
+try:
+    file = open("sample.txt", "r")
+    print(file.read())
+
+except FileNotFoundError:
+    print("File not found.")
+
+finally:
+    print("Program Finished.")
