bd67a1f5f081fe4884d2805b083b3b063547b29b
RIDHIMA MISHRA <mishraridhima65@gmail.com>
2026-07-19 20:24:28 +0530

    Add multiple exception handling program

---
+++ Day14/02_multiple_exceptions.py
@@ -0,0 +1,12 @@
+# Multiple Exceptions
+
+try:
+    a = int(input("Enter first number: "))
+    b = int(input("Enter second number: "))
+    print("Result:", a / b)
+
+except ZeroDivisionError:
+    print("Cannot divide by zero.")
+
+except ValueError:
+    print("Enter valid numbers only.")
