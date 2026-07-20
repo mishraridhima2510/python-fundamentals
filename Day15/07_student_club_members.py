e77ca3a8daa10d9124ce7c6412c7744f5c775a30
RIDHIMA MISHRA <mishraridhima65@gmail.com>
2026-07-20 16:56:32 +0530

    Add student club membership project

---
+++ Day15/07_student_club_members.py
@@ -0,0 +1,8 @@
+# Student Club Membership
+
+coding = {"Aman", "Riya", "Rahul", "Priya"}
+robotics = {"Rahul", "Priya", "Arjun"}
+
+print("Both Clubs:", coding & robotics)
+print("Only Coding:", coding - robotics)
+print("All Members:", coding | robotics)
