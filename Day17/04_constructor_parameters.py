c46bad3a3dba3c25d84318a1bec7ce29ec83ea78
RIDHIMA MISHRA <mishraridhima65@gmail.com>
2026-07-22 17:37:57 +0530

    Add parameterized constructor program

---
+++ Day17/04_constructor_parameters.py
@@ -0,0 +1,12 @@
+# Constructor with Parameters
+
+class Student:
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+student1 = Student("Ridhima", 19)
+
+print(student1.name)
+print(student1.age)
