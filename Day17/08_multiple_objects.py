08e2af84413e02351a6634d1bcbbf236041f406f
RIDHIMA MISHRA <mishraridhima65@gmail.com>
2026-07-22 17:40:31 +0530

    Add multiple objects example

---
+++ Day17/08_multiple_objects.py
@@ -0,0 +1,12 @@
+# Multiple Objects
+
+class Student:
+
+    def __init__(self, name):
+        self.name = name
+
+student1 = Student("Ridhima")
+student2 = Student("Rahul")
+
+print(student1.name)
+print(student2.name)
