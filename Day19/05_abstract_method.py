76705e3abff546afec49bc9a81f541c34e20d638
RIDHIMA MISHRA <mishraridhima65@gmail.com>
2026-07-24 22:26:46 +0530

    Add abstract method example

---
+++ Day19/05_abstract_method.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+
+class Vehicle(ABC):
+
+    @abstractmethod
+    def start(self):
+        pass
+
+class Car(Vehicle):
+
+    def start(self):
+        print("Car Started")
+
+car = Car()
+car.start()
