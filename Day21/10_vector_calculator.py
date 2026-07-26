e8c3bf85e82b94a75432d615040cbb8f2d5a5550
RIDHIMA MISHRA <mishraridhima65@gmail.com>
2026-07-26 21:35:25 +0530

    Add vector calculator mini project using magic methods

---
+++ Day21/10_vector_calculator.py
@@ -0,0 +1,18 @@
+# Vector Calculator
+
+class Vector:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __str__(self):
+        return f"Vector({self.x}, {self.y})"
+
+v1 = Vector(2, 3)
+v2 = Vector(4, 5)
+
+print(v1 + v2)
