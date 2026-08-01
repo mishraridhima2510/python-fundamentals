e6b48da999df8c6344eae121c8f528148321391a
RIDHIMA MISHRA <mishraridhima65@gmail.com>
2026-08-01 23:19:32 +0530

    Add multiple threads example

---
+++ Day28/02_multiple_threads.py
@@ -0,0 +1,15 @@
+# Multiple Threads
+
+import threading
+
+def task(name):
+    print(name, "started")
+
+t1 = threading.Thread(target=task, args=("Thread-1",))
+t2 = threading.Thread(target=task, args=("Thread-2",))
+
+t1.start()
+t2.start()
+
+t1.join()
+t2.join()
